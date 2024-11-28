86a0717820536a776d0bea336f8d7d5393448ae4
Tejas <83050961+Tejas-Solanki3@users.noreply.github.com>
2024-11-28 10:10:38 +0530

    Add files via upload

---
+++ triangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    int b = 5;
+    float area = 0.5*(a*b) ;
+
+    cout<<"The area of triangle is : " <<area;
+
+    return 0;
+}
