222a539dfa44f1b62340947a4f4a5560b40946ed
Tejas <83050961+Tejas-Solanki3@users.noreply.github.com>
2024-12-01 17:37:49 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    cout <<sizeof(int)<<" Bytes"<<endl;
+    cout <<sizeof(char)<<" Bytes"<<endl;
+    cout <<sizeof(float)<<" Bytes"<<endl;
+    cout <<sizeof(double)<<" Bytes"<<endl;
+    cout <<sizeof(string)<<" Bytes";
+}
