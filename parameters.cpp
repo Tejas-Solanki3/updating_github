4cfb702b7bf9db2edb9b46af8f835bb7d0d3f641
Tejas <83050961+Tejas-Solanki3@users.noreply.github.com>
2024-12-12 10:09:14 +0530

    Add files via upload

---
+++ parameters.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+//display
+void displayNum(int n1, int n2) {
+    cout<<"The int number is " <<n2;
+
+}
+
+int main() {
+
+    int num1 = 5;
+    double num2 = 5.5;
+
+    //calling the function
+    displayNum(num1,num2);
+
+    return 0;
+}
